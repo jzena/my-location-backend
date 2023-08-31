d2da8c3829d054887161ed4b68966a9da49e7d96
Wilson Zena <zenazamorawilson@gmail.com>
2023-08-31 11:43:33 -0500

    updating service worker file

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withPWA = require('next-pwa')({
   skipWaiting: true,
   buildExcludes: [/middleware-manifest.json$/],
   disable:process.env.NODE_ENV === 'development',
-  sw: 'sw.js'
+  sw: 'service-worker.js'
 });
 /** @type {import('next').NextConfig} */
 
